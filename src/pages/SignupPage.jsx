7350afe6d9352c71a19daf57704b2e73bdcc0cac
begoczb <begocz.31@gmail.com>
2022-09-30 16:09:03 +0200

    Add Login/Signup Navigation to buttons

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 const SignupPage = () => {
-  return <div>SignupPage</div>;
+  const { state } = useLocation();
+  const { type } = state || {};
+
+  return (
+    <div>
+      <p>{type}</p>
+    </div>
+  );
 };
 
 export default SignupPage;
