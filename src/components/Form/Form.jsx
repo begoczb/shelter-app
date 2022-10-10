f2a0168a1c3469126619c37d43e3d2f7d828c75b
begoczb <begocz.31@gmail.com>
2022-10-10 18:02:54 +0200

    add checkbox logic + axios request

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -3,14 +3,16 @@ import React, { useState } from "react";
 import { API_URL } from "../../utils/constants";
 import axios from "axios";
 import "./Form.css";
+import { useContext } from "react";
+import { AuthContext } from "../../context/auth.context";
 
 const baseURL = API_URL;
 
 const Form = () => {
   const [address, setAddress] = useState("");
   const [guests, setGuests] = useState("");
-  const [gender, setGender] = useState("");
-  const [pet, setPet] = useState("");
+  const [gender, setGender] = useState(false);
+  const [pet, setPet] = useState(false);
   const [startDate, setStartDate] = useState("");
   const [endDate, setEndDate] = useState("");
 
@@ -19,40 +21,56 @@ const Form = () => {
   const handleAddress = (e) => setAddress(e.target.value);
   const handleGuests = (e) => setGuests(e.target.value);
   const handleGender = (e) => setGender(e.target.value);
-  const handlePet = (e) => setPet(e.target.value);
+
   const handleStartDate = (e) => setStartDate(e.target.value);
   const handleEndDate = (e) => setEndDate(e.target.value);
+  console.log(startDate);
 
-  const handleSignupSubmit = (e) => {
+  const { getToken } = useContext(AuthContext);
+
+  const handleAddRoom = (e) => {
     e.preventDefault();
+    const token = getToken();
+    const dateOne = new Date(startDate);
+    const dateTwo = new Date(endDate);
 
-    // axios({
-    //   url: `room`,
-    //   baseURL: baseURL,
-    //   method: "post",
-    //   data: {
-    //     email,
-    //     password,
-    //     firstName,
-    //     lastName,
-    //   },
-    // })
-    //   .then((response) => {
-    //     console.log(response.data);
-    //     navigate("/login");
-    //   })
-    //   .catch((error) => {
-    //     console.log(error);
-    //     // const errorDescription = error.response.data.message;
-    //     // setErrorMessage(errorDescription);
-    //   });
+    axios({
+      url: `room`,
+      baseURL: API_URL,
+      method: "post",
+      headers: { Authorization: `Bearer ${token}` },
+      data: {
+        address,
+        guests,
+        pet,
+        gender,
+        dateOne,
+        dateTwo,
+      },
+    })
+      .then((response) => {
+        console.log(response.data);
+        // navigate("/login");
+      })
+      .catch((error) => {
+        console.log(error);
+        // const errorDescription = error.response.data.message;
+        // setErrorMessage(errorDescription);
+      });
+  };
+
+  const handleCheckbox = (type) => {
+    if (type === "gender") {
+      gender ? setGender(false) : setGender(true);
+    } else {
+      pet ? setPet(false) : setPet(true);
+    }
   };
 
   return (
     <>
       <Container>
-        <form className="form" onSubmit={handleSignupSubmit}>
-
+        <form className="form" onSubmit={handleAddRoom}>
           {/* <label>Address:</label> */}
 
           <input
@@ -78,16 +96,24 @@ const Form = () => {
             type="checkbox"
             name="gender"
             value={gender}
-            onChange={handleGender}
+            onChange={() => {
+              handleCheckbox("gender");
+            }}
           />
 
           <label>Pets autorized:</label>
-          <input type="checkbox" name="pet" value={pet} onChange={handlePet} />
+          <input
+            type="checkbox"
+            name="pet"
+            value={pet}
+            onChange={() => {
+              handleCheckbox("pet");
+            }}
+          />
 
           <label>Start Date:</label>
           <input
             type="date"
-            min="2023-01-01"
             name="startDate"
             min={new Date().toISOString().substring(0, 10)}
             value={startDate}
@@ -102,7 +128,7 @@ const Form = () => {
             onChange={handleEndDate}
           />
 
-          <Button variant="contained" type="submit">
+          <Button variant="contained" type="submit" onClick={handleAddRoom}>
             Add
           </Button>
         </form>
