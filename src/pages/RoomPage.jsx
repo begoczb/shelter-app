eb163a18783cc057d4dbf415a0050e3b0d97174f
cmenasse <cmenasse@student.42.fr>
2022-10-30 15:28:40 +0100

    roominfo

--- src/pages/RoomPage.jsx
+++ src/pages/RoomPage.jsx
@@ -1,11 +1,20 @@
 import { Container } from "@mui/material";
 import React from "react";
+import { useLocation } from "react-router-dom";
+
 
 const RoomPage = () => {
+
+  const { state } = useLocation();
+  const { hostInfo, roomInfo } = state || {};
+
   return (
     <main id="room">
       <Container>
-        <h2>Welcome</h2>
+        <h2>{roomInfo.title}</h2>
+        <ul>
+        <li>{roomInfo.description}</li>
+        </ul>
       </Container>
     </main>
   );
