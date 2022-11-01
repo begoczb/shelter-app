2729a8060458e3475f509e45cdb13f0c2b0da80d
begoczb <begocz.31@gmail.com>
2022-11-01 09:42:29 +0100

    chang constant var

--- src/pages/RoomPage.jsx
+++ src/pages/RoomPage.jsx
@@ -11,15 +11,13 @@ import axios from "axios";
 import { AuthContext } from "../context/auth.context";
 
 const RoomPage = () => {
-
   const { getToken } = useContext(AuthContext);
 
-
   const { state } = useLocation();
-  const { hostInfo, roomInfo } = state || {};
+  let { hostInfo } = state || {};
+  const { roomInfo } = state || {};
 
   const [hostDetails, setHostDetails] = useState("");
-
 
   useEffect(() => {
     if (!hostInfo) {
@@ -36,42 +34,54 @@ const RoomPage = () => {
       };
       getHostDetails();
     }
-    }, []);
-
-
+  }, []);
 
   return (
     <main id="room" style={backgroundStyleGen}>
       <Container className="global">
-      <p>{roomInfo.address.description}</p>
+        <p>{roomInfo.address.description}</p>
 
-      <img src={image} alt="listing"/>
-      <h1>{roomInfo.title}</h1>
-      <h2>{hostInfo.firstName}</h2>
-      <p>{roomInfo.description}</p>
-      
-      <div className="container">
-        <div className="item">
-          <p>I can host <br/> {roomInfo.guests} {roomInfo.guests > 1 ? " people" : " person"}</p>
+        <img src={image} alt="listing" />
+        <h1>{roomInfo.title}</h1>
+        <h2>{hostInfo.firstName}</h2>
+        <p>{roomInfo.description}</p>
+
+        <div className="container">
+          <div className="item">
+            <p>
+              I can host <br /> {roomInfo.guests}{" "}
+              {roomInfo.guests > 1 ? " people" : " person"}
+            </p>
+          </div>
+          <div className="item">
+            <p>
+              Pet friendly <br /> {roomInfo.pets ? "yes" : "no"}
+            </p>
+          </div>
+          <div className="item">
+            <p>
+              Hosts <br /> {roomInfo.gender ? "women only" : "everybody"}{" "}
+            </p>
+          </div>
         </div>
-        <div className="item">
-          <p>Pet friendly <br/> {roomInfo.pets? "yes" : "no"}</p>
+        <div className="container">
+          <div className="item">
+            <p>
+              From <br /> {roomInfo.startDate.substring(0, 10)}
+            </p>
+          </div>
+          <div className="item">
+            <p>
+              To <br /> {roomInfo.endDate.substring(0, 10)}
+            </p>
+          </div>
+          <div className="item">
+            <p>
+              {" "}
+              <strong>Contact </strong>
+            </p>
+          </div>
         </div>
-        <div className="item">
-          <p>Hosts <br/> {roomInfo.gender? "women only" : "everybody"} </p>
-        </div>
-      </div>
-      <div className="container">
-        <div className="item">
-          <p>From <br/> {roomInfo.startDate.substring(0, 10)}</p>
-        </div>
-        <div className="item">
-          <p>To <br/> {roomInfo.endDate.substring(0, 10)}</p>
-        </div>
-        <div className="item">
-          <p> <strong>Contact </strong></p>
-        </div>
-      </div>
       </Container>
     </main>
   );
